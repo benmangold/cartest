e3fdaba089aa220e9aeb0f0fb32d1f2661f830c7
Ben Mangold <benmangold@gmail.com>
2019-10-04 01:06:29 -0400

    addGetAudioListRoute cleanup

--- server/src/routes/addGetAudioListRoute.js
+++ server/src/routes/addGetAudioListRoute.js
@@ -1,6 +1,6 @@
 const { getEncodingJobList } = require('../db/getEncodingJobList.js');
 
-const { getS3 } = require('../aws/getS3')
+const { getS3 } = require('../aws/getS3');
 
 const s3 = getS3();
 
@@ -8,17 +8,17 @@ module.exports.addGetAudioListRoute = app => {
   return app.get('/api/audioLinks', async (req, res) => {
     const list = await getEncodingJobList();
 
-    const links = list.map(item => {
+    const links = list
+      .map(item =>
+        item['uploadData']
+          ? s3.getSignedUrl('getObject', {
+              Bucket: item['uploadData']['Bucket'],
+              Key: item['uploadData']['Key'],
+            })
+          : null
+      )
+      .filter(item => item != null);
 
-      if (item['uploadData']) {
-        const params = { 'Bucket':item['uploadData']['Bucket'], 'Key': item['uploadData']['Key']};
-        const url = s3.getSignedUrl('getObject', params);  
-        return url
-      } 
-    })
-
-    const filtered = links.filter(el => el != null);
-
-    res.end(JSON.stringify(filtered));
+    res.end(JSON.stringify(links));
   });
-}
+};
