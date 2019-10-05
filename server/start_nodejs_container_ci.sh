0064885c7e389a4d09f87eefc511e30622dbff67
Ben Mangold <benmangold@gmail.com>
2019-10-05 17:24:50 -0400

    ci server creds

--- server/start_nodejs_container_ci.sh
+++ server/start_nodejs_container_ci.sh
@@ -10,8 +10,22 @@
 
 cd "$(dirname "$0")"
 
+## set credentials proveded by jenkins
+
+touch aws.secret.js
+
+echo "module.exports = {" >> aws.secret.js
+echo "  AWS_ACCESS_KEY: '$AWS_ACCESS_KEY'," >> aws.secret.js
+echo "  AWS_SECRET_KEY: '$AWS_SECRET_KEY'," >> aws.secret.js
+echo "};" >> aws.secret.js
+echo "" >> aws.secret.js
+
+## build docker container with secret
+
 cd ..
 
 docker build -t bm/nodejs-ffmpeg .
+
+rm server/aws.secret.js
 
 docker run -it -d --name nodejs -e CI_ENV:$CI_ENV --network host bm/nodejs-ffmpeg node server/index.js 
